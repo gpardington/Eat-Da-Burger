6fca81a434591fedaf1099017683bb75353781cf
Grant Pardington <grant.pardington@gmail.com>
2020-09-20 12:31:56 -0600

    Added initializing server app to server.js file

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 //Set dependencies & variables
 const express = require("express");
-
 const PORT = process.env.PORT || 3000;
-
+//Start Express Server
 const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Parse request body 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
