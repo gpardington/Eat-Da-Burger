3be04af61dbf348c29ba894d27bfe2e82af93986
Grant Pardington <grant.pardington@gmail.com>
2020-09-20 15:22:50 -0600

    Adding heroku DB connectors to connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	burger_name varchar(125) NOT NULL,
 	crushed BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
