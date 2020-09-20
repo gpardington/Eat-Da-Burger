de2fc19e846418aa0c80416ddbdc520068b87a9d
Grant Pardington <grant.pardington@gmail.com>
2020-09-20 11:53:15 -0600

    Added seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+### Seeds 
+
+USE burgers_db;
+INSERT INTO burgers (burger_name, crushed)
+VALUES
+('Cheese Burger', FALSE),
+('Bacon Guac Double Cheese Burger', FALSE),
+('Double Royale with Cheese', FALSE),
+('Impossible Burger', FALSE),
+('Nashville Hot Chicken Burger', FALSE)
